494be82de644acf700bd68422cf853d8888560db
Keshav Shah <keshavrshah@gmail.com>
2026-02-16 20:55:13 -0500

    tamagui setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,20 @@
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
+import { TamaguiProvider } from 'tamagui';
+import {config} from '@/constants/tamagui-config' 
 
 export default function RootLayout() {
   return (
     <>
+        <TamaguiProvider config={config} defaultTheme="light">
+
       <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="index" />
       </Stack>
       <StatusBar style="light" />
+          </TamaguiProvider>
+
     </>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { ViroARSceneNavigator } from '@reactvision/react-viro';
 import OpeningScene from '@/components/ar-scenes/OpeningScene';
-
+import { Button } from 'tamagui'
+import { Text } from 'tamagui';
 export default function ARHome() {
   return (
-    <ViroARSceneNavigator
-      initialScene={{ scene: OpeningScene }}
-      style={styles.container}
-    />
+    <View style={{alignItems:"center", justifyContent:"center", flexGrow:1}}>
+      <Button>Lorem ipsum</Button>
+      <Text>123</Text>
+      {/* <ViroARSceneNavigator
+        initialScene={{ scene: OpeningScene }}
+        style={styles.container}
+      /> */}
+    </View>
+
   );
 }
 
